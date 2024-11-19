75c75bec09a63a5a5b96da6fca2e94ac18c1e287
Zero <john.royer@gmail.com>
2024-11-20 07:51:26 +0800

    move build script

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
-# build ubuntu base image
-docker build . -f ubuntu/Dockerfile -t johnroyer/forgejo-runners:forgejo-ubuntu-base
-
 # build Ubuntu 22.04
 docker build . -f ubuntu/22.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-2204
