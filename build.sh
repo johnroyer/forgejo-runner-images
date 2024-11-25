ad4d7a2fdc390bb4c091ce13d89463d4926c0506
Zero <john.royer@gmail.com>
2024-11-25 13:56:41 +0800

    add build script for Ubuntu 24.04

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 # build Ubuntu 22.04
 docker build . -f ubuntu/22.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-2204 --no-cache
+
+# build Ubuntu 24.04
+docker build . -f ubuntu/24.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-2404 --no-cache
