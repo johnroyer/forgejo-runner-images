e37f5e96073c6b9d3eb47b2a43954c767af1b4f7
Zero <john.royer@gmail.com>
2024-11-25 13:55:10 +0800

    build docker without cache

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # build Ubuntu 22.04
-docker build . -f ubuntu/22.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-2204
+docker build . -f ubuntu/22.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-2204 --no-cache
