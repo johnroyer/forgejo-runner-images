cec737b114312c06e85bca15eac10da68ec9639b
Zero <john.royer@gmail.com>
2024-11-30 13:01:08 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# build Ubuntu 20.04
+docker build . -f ubuntu/20.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-2004 --no-cache
+
 # build Ubuntu 22.04
 docker build . -f ubuntu/22.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-2204 --no-cache
 
