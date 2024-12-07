e97eabe701dec582706d35955b8c2ae9aea10fc1
Zero <john.royer@gmail.com>
2024-12-07 10:09:38 +0800

    update build script

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ docker build . -f ubuntu/22.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-22
 
 # build Ubuntu 24.04
 docker build . -f ubuntu/24.04/Dockerfile -t johnroyer/forgejo-runners:ubuntu-2404 --no-cache
+
+# build alpine linux
+docker build . -f alpine/Dockerfile -t johnroyer/forgejo-runners:alpine --no-cache
